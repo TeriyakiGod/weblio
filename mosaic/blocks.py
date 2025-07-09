7a3d8343a778a517252e70c5fe028f1b91f05e88
Kacper Akdag-Ochnik <kacperochnik01@gmail.com>
2025-07-09 18:24:56 +0200

    remove redundant method

--- mosaic/blocks.py
+++ mosaic/blocks.py
@@ -13,12 +13,6 @@ class TileBlock(StructBlock):
         icon = "image"
         template = "mosaic/blocks/tile_block.html"
 
-    def get_admin_display_title(self, value):
-        image_count = len(value.get('images', []))
-        if image_count == 1:
-            return f"Mosaic Tile ({image_count} image)"
-        return f"Mosaic Tile ({image_count} images)"
-
 
 class TilesStreamBlock(StreamBlock):
     tile_block = TileBlock(label="Mosaic Tile")
